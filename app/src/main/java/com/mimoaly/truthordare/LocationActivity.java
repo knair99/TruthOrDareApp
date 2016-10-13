44dd4590468839d05e1e7455ec87fe53534d0c6c
Karunakaran Prasad <kprasad@Karunakarans-MacBook-Air.local>
2016-10-13 01:13:54 -0700

    Added until location screen

--- app/src/main/java/com/mimoaly/truthordare/LocationActivity.java
+++ app/src/main/java/com/mimoaly/truthordare/LocationActivity.java
@@ -1,7 +1,9 @@
 package com.mimoaly.truthordare;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class LocationActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class LocationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        //Set the title bar for choosing location
+        Toolbar topToolbar = (Toolbar) findViewById(R.id.top_toolbar);
+        setSupportActionBar(topToolbar);
+
+
+
+
+
     }
 }
